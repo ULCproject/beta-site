8a640add76448706d21767b8d2c661dbfaec0f7e
Derrick Farris <mr.dcfarris@gmail.com>
2018-04-03 20:49:33 -0500

    Optimize scripts more

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -3,47 +3,34 @@
 
     $(document).on('ready', function() {
 			
-            /* MENU JS */
-    		
-            $(window).scroll(function() {
-                if ($("#mainNav").offset().top > 100) {
-                    $("#mainNav").addClass("navbar-shrink");
-                } else {
-                    $("#mainNav").removeClass("navbar-shrink");
-                }
-            });
-    		
-            $('a.js-scroll-trigger').on('click', function(e) {
-                var anchor = $(this);
-                $('html, body').stop().animate({
-                    scrollTop: $(anchor.attr('href')).offset().top - 48
-                }, 1000);
-                e.preventDefault();
-            });
-    		
-            // Closes responsive menu when a scroll trigger link is clicked
-            $('.js-scroll-trigger').on('click', function() {
-                $('.navbar-collapse').collapse('hide');
-            });
-
-            // Activate scrollspy to add active class to navbar items on scroll
-            $('body').scrollspy({
-                target: '#mainNav',
-                offset: 54
-            });
-
-        /* VENOBOX JS */
-
-        $('.venobox').venobox({
-            numeratio: true,
-            titleattr: 'data-title',
-            titlePosition: 'bottom',
-            spinner: 'wave'
+        /* MENU JS */
+		
+        $(window).scroll(function() {
+            if ($("#mainNav").offset().top > 100) {
+                $("#mainNav").addClass("navbar-shrink");
+            } else {
+                $("#mainNav").removeClass("navbar-shrink");
+            }
+        });
+		
+        $('a.js-scroll-trigger').on('click', function(e) {
+            var anchor = $(this);
+            $('html, body').stop().animate({
+                scrollTop: $(anchor.attr('href')).offset().top - 48
+            }, 1000);
+            e.preventDefault();
+        });
+		
+        // Closes responsive menu when a scroll trigger link is clicked
+        $('.js-scroll-trigger').on('click', function() {
+            $('.navbar-collapse').collapse('hide');
         });
 
-        /* WOW ANIMATION JS */
-
-        new WOW().init();
+        // Activate scrollspy to add active class to navbar items on scroll
+        $('body').scrollspy({
+            target: '#mainNav',
+            offset: 54
+        });
     });
 	
 })(jQuery);
